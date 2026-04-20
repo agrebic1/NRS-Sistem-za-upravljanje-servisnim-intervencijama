9131a3fa76fbd86ff0049b3910885f07dc6b5545
ebegic68 <119817064+ebegic68@users.noreply.github.com>
2026-04-21 00:36:34 +0200

    Kreiranje modula za tipove types.js

---
+++ Projekat/domain/types/types.js
@@ -0,0 +1,22 @@
+//Tipovi 
+
+const StatusZahtjeva = [
+  "novi",
+  "u_obradi",
+  "zavrsen"
+];
+
+const StatusIntervencije = [
+  "planirana",
+  "u_toku",
+  "zavrsena"
+];
+
+const Uloge = [
+  "admin",
+  "dispecer",
+  "serviser",
+  "korisnik"
+];
+
+module.exports = { StatusZahtjeva, StatusIntervencije, Uloge };
